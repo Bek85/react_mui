8af1e8dc47e62ab404fffc385a977c8efa71909a
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-12-03 02:58:56 +0500

    add more fields to Add component & use MUI Snackbar component

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,20 @@
-import { makeStyles, Tooltip, Fab, Modal, Container } from '@material-ui/core';
+import {
+  makeStyles,
+  Tooltip,
+  Fab,
+  Modal,
+  Container,
+  TextField,
+  MenuItem,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  FormLabel,
+  Button,
+  Snackbar,
+} from '@material-ui/core';
 import { AddOutlined } from '@material-ui/icons';
+import Alert from '@material-ui/lab/Alert';
 import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
@@ -23,20 +38,122 @@ const useStyles = makeStyles((theme) => ({
       height: '100vh',
     },
   },
+  form: {
+    padding: theme.spacing(2),
+  },
+  item: {
+    marginBottom: theme.spacing(2),
+  },
 }));
 
 const Add = () => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenAlert(false);
+  };
   return (
     <>
-      <Tooltip title="Add" aria-label="add" onClick={() => setOpen(!open)}>
+      <Tooltip title="Add" aria-label="add" onClick={() => setOpen(true)}>
         <Fab color="primary" className={classes.fab}>
           <AddOutlined />
         </Fab>
       </Tooltip>
-      <Modal open={open} onClick={() => setOpen(!open)}>
-        <Container className={classes.container}>Hello</Container>
+      <Modal open={open}>
+        <Container className={classes.container}>
+          <form className={classes.form} autoComplete="off">
+            <div className={classes.item}>
+              <TextField
+                id="standard=basic"
+                label="Title"
+                size="small"
+                style={{ width: '100%' }}
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField
+                id="outlined-multiline-static"
+                multiline
+                rows={4}
+                defaultValue="Your story goes here..."
+                variant="outlined"
+                label="Description"
+                size="small"
+                style={{ width: '100%' }}
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField select label="Visibility" value="Public">
+                <MenuItem value="Public">Public</MenuItem>
+                <MenuItem value="Private">Private</MenuItem>
+                <MenuItem value="Unlisted">Unlisted</MenuItem>
+              </TextField>
+            </div>
+            <div className={classes.item}>
+              <RadioGroup aria-label="gender" name="Gender">
+                <FormLabel component="legend">Who can Comment?</FormLabel>
+                <FormControlLabel
+                  value="everybody"
+                  control={<Radio size="small" />}
+                  label="Everybody"
+                />
+                <FormControlLabel
+                  value="my Friends"
+                  control={<Radio size="small" />}
+                  label="My Friends"
+                />
+                <FormControlLabel
+                  value="nobody"
+                  control={<Radio size="small" />}
+                  label="Nobody"
+                />
+                <FormControlLabel
+                  value="custom"
+                  disabled
+                  control={<Radio size="small" />}
+                  label="Custom (for premium accounts)"
+                />
+              </RadioGroup>
+            </div>
+            <div className={classes.item}>
+              <Button
+                variant="outlined"
+                color="primary"
+                style={{ marginRight: 20 }}
+                onClick={() => setOpenAlert(true)}
+              >
+                Create
+              </Button>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={() => setOpen(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+            <Snackbar
+              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              open={openAlert}
+              autoHideDuration={6000}
+              onClose={handleClose}
+            >
+              <Alert
+                onClose={handleClose}
+                severity="success"
+                sx={{ width: '100%' }}
+              >
+                A post has been created!!!
+              </Alert>
+            </Snackbar>
+          </form>
+        </Container>
       </Modal>
     </>
   );
